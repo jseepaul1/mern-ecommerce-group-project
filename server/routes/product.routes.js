8602db1dee1b74db1e6f09bb80b980cc48e6aa85
jseepaul1 <seepauljulie@gmail.com>
2022-08-13 16:26:28 -0400

    added product model, controller,routes and admin auth

---
+++ server/routes/product.routes.js
@@ -0,0 +1,31 @@
+const productController = require("../controllers/product.controller");
+const { authenticateToken } = require("../middleware/authenticateToken");
+const { authenticateAdmin } = require("../middleware/authenticateAdmin");
+
+module.exports = (app) => {
+  app.get("/api/products", authenticateToken, productController.getAllProducts);
+  app.get(
+    "/api/products/:id",
+    authenticateToken,
+    productController.getProductById
+  );
+  app.post(
+    "/api/products",
+    authenticateToken,
+    authenticateAdmin,
+    productController.createProduct
+  );
+  app.patch(
+    "/api/products/:id",
+    authenticateToken,
+    authenticateAdmin,
+    productController.updateProduct
+  );
+  app.delete(
+    "/api/products/:id",
+    authenticateToken,
+    authenticateAdmin,
+    productController.deleteProduct
+  );
+};
+
