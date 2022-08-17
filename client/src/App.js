e36dfde10f7f19577023aa0270f063d3ddeb6df3
JBegin63 <100534652+JBegin63@users.noreply.github.com>
2022-08-17 15:21:13 -0400

    login and registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import LoginAndRegistration from './Components/LoginAndRegistration/LoginAndRegistration';
+import Dashboard from './Components/Dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route element={<LoginAndRegistration />} path='/' />
+          <Route element={<Dashboard />} path='/dashboard' />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const Dashboard = () => {
+    const navigate = useNavigate();
+
+    const handleLogout= (e) => {
+        e.preventDefault();
+        axios
+            .post('http://localhost:3000/api/users/logout', { withCredentials: true }) 
+            .then((res) => {
+                console.log(res);
+                navigate('/')
+            })
+            .catch((err) => {
+                console.log(err.response);
+                // setRegisterErrors(err.response);
+            });
+    };
+
+    return (
+    <div>
+        <h1>Dashboard</h1>
+        <button onClick={handleLogout}>Logout</button>
+    </div>
+    )
+}
+
+export default Dashboard
