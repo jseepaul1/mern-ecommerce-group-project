c655f7a4167da8688120798e97011a4c49b57422
jseepaul1 <seepauljulie@gmail.com>
2022-09-04 14:04:37 -0400

    made changes to users orders css

--- client/src/Components/UserOrders/UserOrders.jsx
+++ client/src/Components/UserOrders/UserOrders.jsx
@@ -1,66 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Link } from 'react-bootstrap-icons';
-import Header from '../Header/Header';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Header from "../Header/Header";
+import { useNavigate } from "react-router";
+import "./UserOrders.css"
 
 const UserOrders = () => {
-    const [orders, setOrders] = useState([]);
-    const [products, setProducts] = useState([]);
+  const [orders, setOrders] = useState([]);
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
-    // Get current user's orders
-    useEffect(() => {
-        axios
-            .get(`http://localhost:8000/api/orders/user`, { withCredentials: true })
-            .then((res) => {
-                console.log(res.data);
-                setOrders(res.data);
-                setProducts(res.data.items)
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    }, [])
+  // Get current user's orders
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/api/orders/user`, { withCredentials: true })
+      .then((res) => {
+        console.log(res.data);
+        setOrders(res.data);
+        setProducts(res.data.items);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    return (
-        <div>
-            <Header />
-            <h1>Orders</h1>
-            <div className="container mt-6">
-                <div className="row">
-                    {orders.map((order, index) => (
-                        <div key={index}>
-                            <div className='d-flex justify-content-center align-items-center mb-4'>
-                                <h2>Order {index + 1}  | </h2>
-                                <h6>&nbsp;&nbsp;Total Price: ${order.totalPrice}</h6>
-                            </div>
-                            <div className='d-flex flex-row flex-wrap justify-content-center'>
-                            {order.items.map((product, _id) => (
-                                    <div className='card mx-4' key={_id}>
-                                        <img
-                                            className='card-img-top'
-                                            style={{ width: 'auto', height: '12rem'}}
-                                            src={product.productImage}
-                                            alt='Cardpic'
-                                        />
-                                        <div className='card-body'>
-                                            <div className='card-title'>
-                                                <Link to={`/product/${product._id}`}>
-                                                    <h6>{product.productName}</h6>
-                                                </Link>
-                                            </div>
-                                            <div className='card-text'>
-                                                <h6>${product.price}</h6>
-                                            </div>
-                                        </div>
-                                    </div>
-                            ))}
-                            </div>
-                        </div>
-                    ))}
-                </div>
+  return (
+    <div>
+      <Header />
+      <h1>Your Orders</h1>
+      <div className="container mt-6">
+        <div className="row">
+          {orders.map((order, index) => (
+            <div key={index}>
+              <div className="d-flex flex-row flex-wrap justify-content-center">
+                {order.items.map((product, index) => (
+                  <div
+                    className="card mx-4 product-card"
+                    key={index}
+                    onClick={() => navigate(`/product/${product.productId}`)}
+                  >
+                    <img
+                      className="card-img-top"
+                      style={{ width: "auto", height: "12rem" }}
+                      src={product.productImage}
+                      alt="Cardpic"
+                    />
+                    <div className="card-body">
+                      <div className="card-title">
+                        <h6>{product.productName}</h6>
+                      </div>
+                      <div className="card-text">
+                        <h6>${product.price}</h6>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="d-flex justify-content-center align-items-center mb-4">
+                <h5>Order {index + 1} | </h5>
+                <h6>&nbsp;&nbsp;Total Price: ${order.totalPrice}</h6>
+              </div>
             </div>
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default UserOrders
+export default UserOrders;
